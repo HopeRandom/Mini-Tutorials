822cd7baf1cde98689254fed0294745550f85da6
HopeRandom <HopeRandom@users.noreply.github.com>
2024-11-02 22:39:54 +0000

    inital commit

---
+++ Countdown Timer JS/script.js
@@ -0,0 +1,16 @@
+const startingTime = 10;
+let time = startingTime * 60;
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+}
